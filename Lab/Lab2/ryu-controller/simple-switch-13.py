6401bff9af17d54ee4dc812b7b95e820239a1e18
Đinh Huỳnh Gia Bảo <100998772+xyan-dhgb@users.noreply.github.com>
2025-05-13 19:40:34 +0700

    update(lab2): fix "crazy" line

--- Lab/Lab2/ryu-controller/simple-switch-13.py
+++ Lab/Lab2/ryu-controller/simple-switch-13.py
@@ -90,8 +90,6 @@ class SimpleSwitch13(app_manager.RyuApp):
         if out_port != ofproto.OFPP_FLOOD:
             match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
 
-
-t
             if msg.buffer_id != ofproto.OFP_NO_BUFFER:
                 self.add_flow(datapath, 1, match, actions, msg.buffer_id)
                 return
